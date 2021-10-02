7e6d910902a2fe08f25a6162dea876a3ef29c0c9
Barnabas Daru <darunabas@gmail.com>
2021-10-02 12:19:50 -0500

    updated site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Biogeography in R workshop",
-        "excerpt":"Biogeography in R workshop We are excited to announce the maiden edition of a workshop in biogeographic methods and tools development in the R computing environment. The course will be 6 days in length and take place at the Island campus of Texas A&amp;M University-Corpus Christi from the January 10-15,...","categories": ["Updates"],
+        "excerpt":"Biogeography in R workshop We are excited to announce the maiden edition of a workshop in biogeographic methods and tools development in the R computing environment. The course will take place at Texas A&amp;M University-Corpus Christi from January 10-15, 2022. The workshop is intended for anyone with interest in biogeographic...","categories": ["Updates"],
         "tags": ["link","Post Formats"],
         "url": "/updates/00_Biogeography-R-workshop-announcement/",
         "teaser": null
